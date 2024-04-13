621b91d932d5ecc48ad94b11bab93255829429f1
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-04-13 19:21:00 -0300

    feat: implementado rota para calcular rentabilidade e index

--- app/views.py
+++ app/views.py
@@ -1,13 +1,22 @@
-from app import app
+from app import app, helpers
 from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
-@app.route('/calcular')
+@app.route('/calcular', methods=['POST', ])
 def calcular():
-    return None
+    valor = request.form['valor']
+    dias = request.form['dias']
+    cdi_pago = request.form['cdi']
+    cdi_com_sobras = helpers.calcula_cdi_considerando_sobras(cdi_pago)
+    rentabilidade_valor = helpers.calcula_rentabilidade_periodo_valor(dias, valor, cdi_pago)
+    valor_sobras = helpers.calcula_valor_sobras(valor)
+    return render_template('index.html', 
+                           rentabilidade_paga=rentabilidade_valor, 
+                           cdi_considerando_sobras=cdi_com_sobras,
+                           valor_sobras=valor_sobras)
 
 @app.route('/salvar_simulacao')
 def salvar_simulacao():
