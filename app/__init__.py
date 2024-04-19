f834af7906c3b2592b229ee10c95b9ab7ad16145
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-04-19 19:35:49 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:admin@localhost/db_plataforma_investimentos'
 db = SQLAlchemy(app)
 
